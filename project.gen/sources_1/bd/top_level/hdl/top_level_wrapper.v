701e0cb6a3913297449d927d773db9ca270df7be
DouglasWWolf <douglaswolf@yahoo.com>
2024-04-15 14:35:33 -0700

    No commit message

---
+++ project.gen/sources_1/bd/top_level/hdl/top_level_wrapper.v
@@ -0,0 +1,72 @@
+//Copyright 1986-2021 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2021.1 (lin64) Build 3247384 Thu Jun 10 19:36:07 MDT 2021
+//Date        : Mon Apr 15 14:19:00 2024
+//Host        : simtool-5 running 64-bit Ubuntu 20.04.6 LTS
+//Command     : generate_target top_level_wrapper.bd
+//Design      : top_level_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module top_level_wrapper
+   (led_green_l,
+    led_orange_l,
+    qsfp0_clk_clk_n,
+    qsfp0_clk_clk_p,
+    qsfp0_gt_grx_n,
+    qsfp0_gt_grx_p,
+    qsfp0_gt_gtx_n,
+    qsfp0_gt_gtx_p,
+    qsfp1_clk_clk_n,
+    qsfp1_clk_clk_p,
+    qsfp1_gt_grx_n,
+    qsfp1_gt_grx_p,
+    qsfp1_gt_gtx_n,
+    qsfp1_gt_gtx_p);
+  output [3:0]led_green_l;
+  output [3:0]led_orange_l;
+  input qsfp0_clk_clk_n;
+  input qsfp0_clk_clk_p;
+  input [3:0]qsfp0_gt_grx_n;
+  input [3:0]qsfp0_gt_grx_p;
+  output [3:0]qsfp0_gt_gtx_n;
+  output [3:0]qsfp0_gt_gtx_p;
+  input qsfp1_clk_clk_n;
+  input qsfp1_clk_clk_p;
+  input [3:0]qsfp1_gt_grx_n;
+  input [3:0]qsfp1_gt_grx_p;
+  output [3:0]qsfp1_gt_gtx_n;
+  output [3:0]qsfp1_gt_gtx_p;
+
+  wire [3:0]led_green_l;
+  wire [3:0]led_orange_l;
+  wire qsfp0_clk_clk_n;
+  wire qsfp0_clk_clk_p;
+  wire [3:0]qsfp0_gt_grx_n;
+  wire [3:0]qsfp0_gt_grx_p;
+  wire [3:0]qsfp0_gt_gtx_n;
+  wire [3:0]qsfp0_gt_gtx_p;
+  wire qsfp1_clk_clk_n;
+  wire qsfp1_clk_clk_p;
+  wire [3:0]qsfp1_gt_grx_n;
+  wire [3:0]qsfp1_gt_grx_p;
+  wire [3:0]qsfp1_gt_gtx_n;
+  wire [3:0]qsfp1_gt_gtx_p;
+
+  top_level top_level_i
+       (.led_green_l(led_green_l),
+        .led_orange_l(led_orange_l),
+        .qsfp0_clk_clk_n(qsfp0_clk_clk_n),
+        .qsfp0_clk_clk_p(qsfp0_clk_clk_p),
+        .qsfp0_gt_grx_n(qsfp0_gt_grx_n),
+        .qsfp0_gt_grx_p(qsfp0_gt_grx_p),
+        .qsfp0_gt_gtx_n(qsfp0_gt_gtx_n),
+        .qsfp0_gt_gtx_p(qsfp0_gt_gtx_p),
+        .qsfp1_clk_clk_n(qsfp1_clk_clk_n),
+        .qsfp1_clk_clk_p(qsfp1_clk_clk_p),
+        .qsfp1_gt_grx_n(qsfp1_gt_grx_n),
+        .qsfp1_gt_grx_p(qsfp1_gt_grx_p),
+        .qsfp1_gt_gtx_n(qsfp1_gt_gtx_n),
+        .qsfp1_gt_gtx_p(qsfp1_gt_gtx_p));
+endmodule
